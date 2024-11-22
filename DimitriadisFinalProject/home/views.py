ca85b72b792a0cb5000ebd82daae03999a01903f
IDimitriadis <ioannis.dimitriadis@acg.edu>
2024-11-22 17:32:03 +0200

    adding basic home page for a start

---
+++ DimitriadisFinalProject/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
+from datetime import datetime
+
+def home(request):
+    return HttpResponse({'Today': datetime.today()})
+
+
+#return render (request, 'home/welcome.html', {'Today': datetime.today()})
